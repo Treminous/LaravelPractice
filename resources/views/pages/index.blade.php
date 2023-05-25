7e980241902a9129950fcddcf912bf328816db3b
Milka Wanga <bakhuyamilka@gmail.com>
2023-05-25 14:17:01 +0300

    Invoked LaravelProject from .env file

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>{{config('app.name',Laravel)}}</title>
     <h1>Index Page</h1>
     <p style="color:red;"> Access by controller</p>
 </head>
